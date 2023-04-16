246e3cfd92f1dbe99f9ddf3c75ea7c7bdea2ca2c
ParasiteDelta <ParasiteDelta@github.com>
2023-04-16 11:37:05 -0600

    Refactored, split it up a little, appears to work

---
+++ src/res.rs
@@ -0,0 +1,5 @@
+pub const NUM_OF_PROBLEMS: &str = "\nPlease enter the number of problems you would like to generate:\n";
+pub const LOWEST_NUM: &str = "\nPlease enter the lowest number you want in the generation range:\n";
+pub const HIGHEST_NUM: &str = "\nPlease enter the highest number you want in the generation range:\n";
+pub const NEGATIVES: &str = "\nWould you like to allow negative numbers for the problem results?\nPlease enter 'y' or 'n':\n";
+pub const WS_PROMPT: &str = "\nPlease enter the operations that you wish to perform.\nSeparate your inputs with commas if selecting multiple, e.g. '1,2,3,4':\n";
